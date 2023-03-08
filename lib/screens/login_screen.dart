29a55c196e3d4fa24e1497fa59151e83f3736c5c
Hazard22 <84402127+Hazard22@users.noreply.github.com>
2023-03-08 00:38:34 -0600

    Practica 2

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rojas_johan_practicas/responsive.dart';
 import 'package:rojas_johan_practicas/widgets/loading_modal_widget.dart';
 import 'package:social_login_buttons/social_login_buttons.dart';
 
@@ -58,9 +59,8 @@ class _LoginScreenState extends State<LoginScreen> {
                   fontSize: 18, decoration: TextDecoration.underline))),
     );
 
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Stack(
+    Widget dataLogin(bool isDesktop) {
+      return Stack(
         children: [
           Container(
             constraints: BoxConstraints.expand(),
@@ -70,7 +70,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     opacity: .3,
                     fit: BoxFit.cover)),
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(30.0),
               child: Stack(
                 alignment: Alignment.topCenter,
                 children: [
@@ -93,19 +93,81 @@ class _LoginScreenState extends State<LoginScreen> {
                     ],
                   ),
                   Positioned(
-                    width: 300,
-                    height: 200,
-                    top: 70,
-                    child: Image.asset(
-                      'assets/images/logo_dc.png',
-                      height: 250,
-                    ),
-                  )
+                      width: 300,
+                      height: 200,
+                      top: 70,
+                      child: isDesktop
+                          ? Image.asset(
+                              'assets/images/logo_dc.png',
+                              height: 250,
+                            )
+                          : Image.asset(
+                              'assets/images/txtTitulo.png',
+                              height: 250,
+                            ))
                 ],
               ),
             ),
           ),
           isLoading ? const LoadingModalWidget() : Container()
+        ],
+      );
+    }
+
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Responsive(
+            desktop: LoginScreenDesktop(
+              dataLogin: dataLogin(false),
+            ),
+            mobile: LoginScreenMobile(
+              dataLogin: dataLogin(true),
+            )));
+  }
+}
+
+class LoginScreenMobile extends StatelessWidget {
+  const LoginScreenMobile({super.key, required this.dataLogin});
+
+  final dataLogin;
+
+  @override
+  Widget build(BuildContext context) {
+    return dataLogin;
+  }
+}
+
+class LoginScreenDesktop extends StatelessWidget {
+  const LoginScreenDesktop({super.key, required this.dataLogin});
+
+  final dataLogin;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/fondo2.jpg'),
+                      opacity: .3,
+                      fit: BoxFit.cover)),
+              child: Center(
+                  child: Image.asset(
+                'assets/images/logo_dc.png',
+                height: 350,
+              )),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: dataLogin,
+          )
         ],
       ),
     );
